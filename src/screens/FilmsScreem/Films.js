240da4b5d700bc407f73b98cc6fc4272629a32e8
AndreLucasv10 <andrelucasv10@gmail.com>
2022-10-16 20:40:38 -0300

    ligando tela films com api

--- src/screens/FilmsScreem/Films.js
+++ src/screens/FilmsScreem/Films.js
@@ -1,31 +1,41 @@
+import React, {useState} from 'react'
+import api from '../../api/api'
+import { useEffect } from 'react'
 import styles from './Films.module.css'
 import img from './Kemushi_no_Boro_pôster.png'
 import img2 from './Meuamigototoro.png'
 import img3 from './Ocasteloanimado.png'
 export default function Films(){
+   
+    const [films, setFilms] = useState([])
+    console.log(films)
+    useEffect(() => {
+        api.get("/films")
+        .then((response) => {
+            setFilms(response.data)
+        })
+        .catch(() =>{
+            console.log("Deu tudo errado")
+        })
+
+     }, [])
     
     return(
+        
         <div className={styles.divfilms} >
-            <h1 className={styles.h1}>Estúdio Ghibli</h1>
-            <p className={styles.ptex1}>Lista de filmes</p>
-            
-            <div className={styles.divbox}>
+            {films.map((films, key) => {
+                return(
 
-            <div className={styles.divimg}>
-            <img className={styles.img} src={img}/>
-            <p className={styles.p}>Detalhes</p>
-            </div>
+                     <div className={styles.divimg}>
+                        <div className={styles.title}><h1>{films.title}</h1></div>
+                            <img className={styles.img} src={films.image}/>
+                                <div className={styles.containerdesc}>
+                                <p/>{films.description}
+                                </div>
+                    </div>
 
-            <div className={styles.divimg}>
-            <img className={styles.img} src={img2}/>
-            <p className={styles.p}>Detalhes</p>
-            </div>
-
-            <div className={styles.divimg}>
-            <img className={styles.img} src={img3}/>
-            <p className={styles.p}>Detalhes</p>
-            </div>
-            </div>
+                )
+            })}
         </div>
     )
 }
