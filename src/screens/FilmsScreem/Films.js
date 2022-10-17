d304e4628c00c93735e633164431dd7f22bc6336
AndreLucasv10 <andrelucasv10@gmail.com>
2022-10-17 15:18:46 -0300

    linkando as paginas

--- src/screens/FilmsScreem/Films.js
+++ src/screens/FilmsScreem/Films.js
@@ -2,9 +2,7 @@ import React, {useState} from 'react'
 import api from '../../api/api'
 import { useEffect } from 'react'
 import styles from './Films.module.css'
-import img from './Kemushi_no_Boro_pôster.png'
-import img2 from './Meuamigototoro.png'
-import img3 from './Ocasteloanimado.png'
+import { Link } from 'react-router-dom'
 export default function Films(){
    
     const [films, setFilms] = useState([])
@@ -22,20 +20,34 @@ export default function Films(){
     
     return(
         
+        
         <div className={styles.divfilms} >
+                <div className={styles.divbuttom}>
+                <Link to={'/'}><button type='button' className={styles.button}>Home</button></Link>
+                <button type='button' className={styles.button}>Sobre</button>
+                <button type='button' className={styles.button}>Filmes</button>
+                <button type='button' className={styles.button}>Personagens</button>
+                </div>
+                <div className={styles.divh1}>
+                    <h1 className={ styles.h1}>
+                        Lista de filmes
+                    </h1>
+                </div>
             {films.map((films, key) => {
-                return(
-
+                                        return(
+                    
+                                   
                      <div className={styles.divimg}>
-                        <div className={styles.title}><h1>{films.title}</h1></div>
+        
+                            <div className={styles.h1}><title>Lista de filmes</title></div>
                             <img className={styles.img} src={films.image}/>
-                                <div className={styles.containerdesc}>
-                                <p/>{films.description}
+                            <p/>{films.description}                               
                                 </div>
-                    </div>
-
+                                    
                 )
-            })}
+            }
+                )
+                    }
         </div>
     )
 }
