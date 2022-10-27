41995162831441db0cb5793d54ecd73d92b77fbc
Charles Junior <selrahcjunior@hotmail.com>
2022-10-27 11:12:35 -0300

    Criando o componente Header

---
+++ src/components/headerComponent/Header.js
@@ -0,0 +1,21 @@
+import img from '../../assets/homescreen/logo.png';
+import styles from './header.module.css';
+import { Link } from "react-router-dom";
+
+export default function Header(){
+    return(
+        <>
+            <div className={styles.botoesConteiner}>
+                <Link to={'/'}><button type='button' className={styles.button}>Home</button></Link>
+                <button type='button' className={styles.button}>Sobre</button>
+                <Link to={'/Films'}><button type='button' className={styles.button}>Filmes</button></Link> 
+                <Link to={'/Characters'}><button type='button' className={styles.button}>Personagens</button></Link>  
+            </div>
+            <div className={styles.divGotoGhibliSite}>
+                <Link to={'/'}>
+                    <img src={img} className={styles.imgStudio}/>
+                </Link>
+            </div>
+        </>
+    )
+}
