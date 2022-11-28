bb4a2138a1d4a1f388d3f53276d3f48f0dac1e61
Charles Junior <selrahcjunior@hotmail.com>
2022-11-28 16:10:45 -0300

    Desinstalando bootstrap errado

--- src/components/headerComponent/Header.js
+++ src/components/headerComponent/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
     return(
         <>
             <div className={styles.botoesConteiner}>
-                <Link to={'/'}><button type='button' className={styles.button}>Home</button></Link>
+                <Link to={'/'}><button className={styles.button}>Home</button></Link>
                 <Link to={'/About'}><button type='button' className={styles.button}>Sobre</button></Link>
                 <Link to={'/Films'}><button type='button' className={styles.button}>Filmes</button></Link> 
                 <Link to={'/Characters'}><button type='button' className={styles.button}>Personagens</button></Link>  
