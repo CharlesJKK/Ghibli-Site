271b137b52ad25c96afcb130391fd6f1b9b3f18c
Charles Junior <selrahcjunior@hotmail.com>
2022-11-29 11:41:12 -0300

    Adicionando tailwind na home e no componente

--- src/components/headerComponent/Header.js
+++ src/components/headerComponent/Header.js
@@ -1,14 +1,13 @@
-import styles from './header.module.css'
 import { Link } from "react-router-dom"
 
 export default function Header(){
     return(
         <>
-            <div className={styles.botoesConteiner}>
-                <Link to={'/'}><button type='button' className={styles.button}>Home</button></Link>
-                <Link to={'/About'}><button type='button' className={styles.button}>Sobre</button></Link>
-                <Link to={'/Films'}><button type='button' className={styles.button}>Filmes</button></Link> 
-                <Link to={'/Characters'}><button type='button' className={styles.button}>Personagens</button></Link>  
+            <div class="w-full text-end mr-5">
+                <Link to={'/'}><button type='button' class="shadow-greenxl mt-1p mr-5p text-xl text-white font-GhibliNormal cursor-pointer rounded-3xl border-pink-600 bg-gradient-to-r from-blue-900 to-green-700 p-1 hover:scale-110">Home</button></Link>
+                <Link to={'/About'}><button type='button' class="shadow-greenxl p-1 mt-1p mr-5p text-xl text-white font-GhibliNormal bg-gradient-to-r from-blue-900 to-green-700 cursor-pointer rounded-3xl border-pink-600 hover:scale-110">Sobre</button></Link>
+                <Link to={'/Films'}><button type='button' class="shadow-greenxl p-1 mt-1p mr-5p text-xl text-white font-GhibliNormal bg-gradient-to-r from-blue-900 to-green-700 cursor-pointer rounded-3xl border-pink-600 hover:scale-110">Filmes</button></Link> 
+                <Link to={'/Characters'}><button type='button' class="shadow-greenxl p-1 mt-1p mr-5p text-xl text-white font-GhibliNormal bg-gradient-to-r from-blue-900 to-green-700 cursor-pointer rounded-3xl border-pink-600 hover:scale-110">Personagens</button></Link>  
             </div>
         </>
     )
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,27 +1,25 @@
-import styles from './homescreen.module.css';
 import img2 from '../../assets/homescreen/teste.png';
 import Header from '../../components/headerComponent/Header';
 
 export default function Homescreen(){
-
     return(
         <div class="bg-gradient-to-br from-blue-500 to-green-400 w-full h-screen overflow-x-hidden">
             <Header/>
             <div>
             </div>
             <div class="mt-44 w-1/3 ml-4">
-                <p className={styles.filmName}>A viagem de chihiro
-                    <p className={styles.filmTxt}>
+                <p class="font-GhibliNormal text-white text-4xl uppercase">A viagem de chihiro
+                    <p class="text-lg text-pink-300 font-GhibliBold">
                         Chihiro e seus pais estão se mudando para uma cidade diferente. A caminho da nova casa, o pai decide pegar um atalho. Eles se deparam com uma mesa repleta de comida, embora ninguém esteja por perto. Chihiro sente o perigo, mas seus pais começam a comer. Quando anoitece, eles se transformam em porcos. Agora, apenas Chihiro pode salvá-los.
                     </p>
                 </p>
                 <a href='https://www.netflix.com/br/title/60023642' target={'_blank'} rel="noopener noreferrer">
-                    <button className={styles.filmButton}>
+                    <button class="ml-65p p-2 w-auto cursor-pointer shadow-3xl bg-gradient-to-r from-red-600 to-red-800 border-red-600 rounded-3xl">
                         Watch on Netflix
                     </button>
                 </a>
                 
-            <img src={img2} className={styles.imgCenter} alt='chihiro correndo'/>
+            <img src={img2} class="w-96 ml-100p min-w-300 mt-75p" alt='chihiro correndo'/>
             </div>
         </div>
     )
