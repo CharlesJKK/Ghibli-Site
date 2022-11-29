89bad54fdf8f1def2fdb081d2488fc675d97aab5
AndreLucasv10 <andrelucasv10@gmail.com>
2022-11-29 09:54:48 -0300

    tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'fundo': "url('/src/screens/FilmsScreem/fundoHome.png')"
+      }
+    },
   },
   plugins: [],
 }
