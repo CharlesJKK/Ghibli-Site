5eb3146a14aea371cbd444c70584095eee0a21f5
Charles Junior <selrahcjunior@hotmail.com>
2022-11-29 11:23:50 -0300

    tailwindconfigs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'GhibliNormal': ['Ghibli'],
+      'GhibliBold': ['GhibliBold']
+    },
     extend: {
+      boxShadow: {
+      '3xl': '3px 2px 1px 0px rgb(0,0,0)',
+      'greenxl': '3px 2px 1px 0px rgb(16, 179, 168)'
+    },
       backgroundImage: {
         'fundo': "url('/src/screens/FilmsScreem/fundoHome.png')"
+      },
+      margin: {
+        '1p': '1%',
+        '5p': '5%',
+        '65p': '65%',
+        '100p': '100%',
+        '75p': '-75%'
       }
     },
   },
